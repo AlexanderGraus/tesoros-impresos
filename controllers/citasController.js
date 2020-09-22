0137edcb26d60c500e028db947403fe8d4caa8cf
Alexander Graus <alegraus31@gmail.com>
2020-09-22 12:56:10 -0300

    creacion de citasController

---
+++ controllers/citasController.js
@@ -0,0 +1,25 @@
+const citasModel = require('../models/citasModel');
+
+module.exports = {
+    getAll: async(req,res,next) =>{
+        const citas = await citasModel.find({});
+        res.json(citas);
+    },
+    getById: async(req,res,next) =>{
+        const cita = await citasModel.findById(req.params.id);
+        res.json(cita);
+    },
+    create: async(req,res,next) =>{
+        const cita = new citasModel(req.body);
+        await cita.save();
+        res.json(cita);
+    },
+    update: async (req,res,next) =>{
+        const cita = await citasModel.update({_id: req.params.id},req.body, {multi:false});
+        res.json(cita);
+    },
+    delete: async (req,res,next) =>{
+        const cita = await citasModel.deleteOne({_id: req.params.id});
+        res.json(cita);
+    }
+};
