c725455de8599db01e2d2ef407f354aeb8615449
Alexander Graus <alegraus31@gmail.com>
2020-09-22 13:14:41 -0300

    llamado a usariosrController en la ruta users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const usersController = require('../controllers/usariosController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', usersController.getAll);
+
+router.get('/:id',usersController.getById);
+
+router.post('/',usersController.create);
+
+router.put('/:id',usersController.update);
+
+router.delete(':id', usersController.delete);
 
 module.exports = router;
