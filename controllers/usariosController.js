f4808494828ea03f791148c63325d32e323b60f7
Alexander Graus <alegraus31@gmail.com>
2020-09-22 13:01:52 -0300

    creacion de delete en usuariosController

---
+++ controllers/usariosController.js
@@ -0,0 +1,25 @@
+const userModel = require('../models/usuariosModel');
+
+module.exports = {
+    getAll: async(req,res,next) =>{
+        const usarios = await userModel.find({});
+        res.json(usarios);
+    },
+    getById: async(req,res,next) =>{
+        const user = await userModel.findById(req.params.id);
+        res.json(user);
+    },
+    create: async(req,res,next) =>{
+        const user = new userModel(req.body);
+        await user.save();
+        res.json(user);
+    },
+    update: async (req,res,next) =>{
+        const user = await userModel.update({_id: req.params.id},req.body, {multi:false});
+        res.json(user);
+    },
+    delete: async (req,res,next) =>{
+        const user = await userModel.deleteOne({_id: req.params.id});
+        res.json(user);
+    }
+};
