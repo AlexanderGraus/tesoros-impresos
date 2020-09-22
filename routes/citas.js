5cf7cc22858d1408a82c01b7ba9b0aabf66f016b
Alexander Graus <alegraus31@gmail.com>
2020-09-22 13:14:05 -0300

    llamado a citasController en la ruta citas

--- routes/citas.js
+++ routes/citas.js
@@ -1,32 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const serviceCitas = require('./../models/citasModel');
+const citasController = require('./../controllers/citasController');
 
-// const citas =[
-//     {id: 1, autor: 'Alexander', libro: 'libro 1',cita: 'No aguanto mas la cuarentena', titulo: 'Sobre la soledad'},
-//     {id: 2, autor: 'Pedro', libro: 'libro 2', cita: 'Que bueno estar vivo', titulo: 'Sobre la vida'}
-// ];
+router.get('/',citasController.getAll);
 
-router.get('/',async (req,res)=>{
-    try {
-        const citas = await serviceCitas.getCitas();
-        res.render('citas',{title: 'Citas Literarias', citas});
-    } catch (error) {
-        console.log(error);
-        res.render('citas',{title: 'Citas Literarias', message: 'Error en la conexion  a la Base de Datos'});
-    }
-});
+router.get('/:id',citasController.getById);
 
-router.get('/:id',async (req,res)=>{
-    try {
-        const idCita = req.params.id;
-        const cita = await serviceCitas.getCita(idCita);
-        // const cita = citas.find(cita => cita.id == idCita);
-        res.render('cita',{title: 'Citas Literarias', cita});
-        
-    } catch (error) {
-        console.log(error);
-    }
-});
+router.post('/',citasController.create);
+
+router.put('/:id',citasController.update);
+
+router.delete('/:id',citasController.delete);
 
 module.exports = router;
